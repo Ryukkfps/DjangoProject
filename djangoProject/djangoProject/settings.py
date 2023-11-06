8e431c0bcfcc568cde36eeddef3c01019ae5d26e
Ryukkfps <kartikey.kg.888@gmail.com>
2023-11-06 13:15:03 +0530

    new Functionality added

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -136,3 +136,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kartikey.kg.888@gmail.com'
+EMAIL_HOST_PASSWORD = 'ofkg qpox kozn buhb'
